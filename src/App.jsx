0216f9801e80e5c260c023cb6754af7a2b149a6b
Glenda Lim <freyjaglenda@gmail.com>
2025-05-13 13:56:43 +0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import { Navbar, Container } from 'react-bootstrap';
-import AnimeSearchPage from './pages/AnimeSearchPage';
-import AnimeDetailsPage from './pages/AnimeDetailsPage';
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import AnimeSearchPage from "./pages/AnimeSearchPage";
+import AnimeDetailsPage from "./pages/AnimeDetailsPage";
+
+function Layout() {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand className="ms-3">Anime Search App</Navbar.Brand>
+      </Navbar>
+      <Outlet />
+    </>
+  );
+}
 
 export default function App() {
-  function Layout() {
-   return (
-      <div>
-        <Navbar expand="lg" className="bg-primary">
-          <Container fluid>
-            <Navbar.Brand className="text-white">Anime Search App</Navbar.Brand>
-          </Container>
-        </Navbar>
-        <Outlet />
-      </div>
-    ) 
-  }
-
   return (
     <BrowserRouter>
       <Routes>
@@ -26,6 +24,6 @@ export default function App() {
           <Route path="*" element={<AnimeSearchPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>  
+    </BrowserRouter>
   );
 }
